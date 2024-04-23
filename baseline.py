49a582e280169db1a346ba2cd19aa6b581b35aa9
Jose Geraldo Fernandes macaverde604@gmail.com <macaverde604@gmail.com>
2024-04-23 03:48:21 +0000

    add long baseline experiment

---
+++ baseline.py
@@ -0,0 +1,32 @@
+
+# # setup
+import torch
+import os
+
+from dataloader import Cifar10
+from runners.baseline import Runner
+
+# # init
+model_label = 'resnet50'
+model = torch.hub.load('pytorch/vision:v0.10.0', model_label, pretrained = False)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+EPOCHS = 100
+BATCH_SIZE = 128
+NUM_WORKERS = 6
+
+trn_ds = Cifar10(split = 'train')
+trn_loader = torch.utils.data.DataLoader(trn_ds, batch_size = BATCH_SIZE,
+                                          shuffle = True, num_workers = NUM_WORKERS)
+
+val_ds = Cifar10(split = 'val')
+val_loader = torch.utils.data.DataLoader(val_ds, batch_size = BATCH_SIZE,
+                                         shuffle = False, num_workers = NUM_WORKERS)
+
+runner = Runner(device, model, model_label)
+
+# # train
+runner.train(EPOCHS, trn_loader, val_loader)
+
+# # eval
+acc = runner.acc(val_loader)
